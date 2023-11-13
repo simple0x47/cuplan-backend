4f5aff427e994afcf43bc0bd0b457564dc595b59
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-13 15:15:03 +0100

    + latest interface of Error

--- src/core/BitwardenSecretsManager.cpp
+++ src/core/BitwardenSecretsManager.cpp
@@ -18,8 +18,7 @@ Result<std::string, Error> BitwardenSecretsManager::getSecret(
     const std::unique_ptr<std::string> secretId) {
   if (secretId == nullptr) {
     return Result<std::string, Error>::err(std::make_unique<Error>(
-        Error(std::make_unique<std::string>(INVALID_SECRET_ID),
-              std::make_unique<std::string>("'secretId' is a nullptr."))));
+        Error(INVALID_SECRET_ID, "'secretId' is a nullptr.")));
   }
 
   const std::string command =
@@ -30,8 +29,7 @@ Result<std::string, Error> BitwardenSecretsManager::getSecret(
 
   if (!pipe) {
     return Result<std::string, Error>::err(std::make_unique<Error>(
-        Error(std::make_unique<std::string>(FAILED_TO_RUN_COMMAND),
-              std::make_unique<std::string>("failed to run 'bws' command."))));
+        Error(FAILED_TO_RUN_COMMAND, "failed to run 'bws' command.")));
   }
 
   while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
@@ -41,10 +39,9 @@ Result<std::string, Error> BitwardenSecretsManager::getSecret(
   const int exitCode = pclose(pipe);
 
   if (exitCode != 0) {
-    return Result<std::string, Error>::err(std::make_unique<Error>(Error(
-        std::make_unique<std::string>(COMMAND_EXITED_WITH_NON_ZERO_CODE),
-        std::make_unique<std::string>("'bws' exited with a non zero code: " +
-                                      result))));
+    return Result<std::string, Error>::err(std::make_unique<Error>(
+        Error(COMMAND_EXITED_WITH_NON_ZERO_CODE,
+              "'bws' exited with a non zero code: " + result)));
   }
 
   nlohmann::json jsonResult = nlohmann::json::parse(result);
