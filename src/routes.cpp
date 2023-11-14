a1ca4b8b119245f789b07e4634368057249d97c1
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-14 02:46:14 +0100

    + example route

--- src/routes.cpp
+++ src/routes.cpp
@@ -1,3 +1,6 @@
 #include "routes.h"
 
-void initializeRoutes(crow::SimpleApp &app) {}
+void initializeRoutes(crow::SimpleApp &app) {
+  CROW_ROUTE(app, "/add/<int>/<int>")
+  ([](int a, int b) { return std::to_string(a + b); });
+}
