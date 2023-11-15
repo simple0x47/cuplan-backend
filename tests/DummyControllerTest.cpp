fd9b8a4b93b17dd6db7061ffa368cb7a4defdbe8
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-15 16:15:43 +0100

    + switch to 'cpr' instead of 'libcurl'

--- tests/DummyControllerTest.cpp
+++ tests/DummyControllerTest.cpp
@@ -1,11 +1,8 @@
 #include "DummyControllerTest.h"
 
 #include <chrono>
-#include <curl/curl.h>
+#include <cpr/cpr.h>
 #include <thread>
-
-size_t writeCallback(void *contents, size_t size, size_t nmemb,
-                     std::string *output);
 
 TEST_F(DummyControllerTest, ReceivesReplyCorrectly) {
   core::ConfigurationReader configReader = getConfigurationReader();
@@ -15,44 +12,14 @@ TEST_F(DummyControllerTest, ReceivesReplyCorrectly) {
   std::this_thread::sleep_for(std::chrono::seconds(
       config["DummyControllerTest"]["SleepWaitingForApiRunner"]));
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+  std::string url =
+      std::string(config["DummyControllerTest"]["Url"]) + std::string("/dummy");
 
-  CURL *curl = curl_easy_init();
-  std::string responseData;
-  CURLcode response = CURLE_FAILED_INIT;
+  cpr::SslOptions sslOptions =
+      cpr::Ssl(cpr::ssl::VerifyHost{false}, cpr::ssl::VerifyPeer{false},
+               cpr::ssl::VerifyStatus{false});
+  cpr::Response response = cpr::Get(cpr::Url{std::move(url)}, sslOptions);
 
-  if (curl) {
-    const std::string url = std::string(config["DummyControllerTest"]["Url"]) +
-                            std::string("/dummy");
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYSTATUS, 0L);
-
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseData);
-
-    response = curl_easy_perform(curl);
-
-    curl_easy_cleanup(curl);
-  }
-
-  curl_global_cleanup();
-
-  if (response != CURLE_OK) {
-    std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(response)
-              << std::endl;
-    FAIL();
-  }
-
-  ASSERT_EQ(std::string("dummy"), responseData);
-}
-
-size_t writeCallback(void *contents, size_t size, size_t nmemb,
-                     std::string *output) {
-  const size_t totalSize = size * nmemb;
-  output->append(static_cast<char *>(contents), totalSize);
-
-  return totalSize;
+  ASSERT_EQ(200L, response.status_code);
+  ASSERT_EQ(std::string("dummy"), response.text);
 }
